3dc3d04702014502f01c9076bc7dbf42890cfe1f
solARis <nitishraosnr2001@gmail.com>
2024-06-21 14:57:44 +0530

    changed gemini api client from langchain to google-generativeai

--- website/hospital/chatbot/pred.py
+++ website/hospital/chatbot/pred.py
@@ -1,21 +1,21 @@
-from langchain_google_genai import GoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+import google.generativeai as genai
 import os
 
-llm = GoogleGenerativeAI(google_api_key = os.environ["best_offer"], model=os.environ["model"])
+genai.configure(api_key=os.environ["best_offer"])
 
-intent_prompt = PromptTemplate.from_template('''so i want you to recognise the intent of the sentence on the basis of a hospital website chatbot, and the intents are,
+common_prompt = '''so i want you to recognise the intent of the sentence on the basis of a hospital website chatbot, and the intents are,
 a = "asking to book a doctors appointment/session", 
 b = "reschedule a previous booked appointment/session", 
 c = "cancel a previous booked apointment/session", 
 d = "if the person is tring to greet the chatbot",
 e = "if the person is trying to provide a feedback/complaint of an earlier hospital visit", 
 f = "or the intent is not matching to niether these previous intents and it is a sentense not related to this topic basically a fallback intent",
-based on the above info only respond if it is a, b, c, d, e or f intent as i mentioned above, the sentence is "{intent}" ''')
+based on the above info only respond if it is a, b, c, d, e or f intent as i mentioned above, the sentence is "{intent}" '''
 
 intents = {'a' : 'book_session', 'b' : 'session_reschedule', 'c' : 'session_cancelation', 'd' : 'greeting', 'e' : 'feedback', 'f' : 'fallback'}
 
 def predict_intent(text):
-    chain = intent_prompt | llm
-    ans = chain.invoke(text).strip().lower().replace('*','')
-    return intents[ans]
+    full_prompt = common_prompt.format(intent=text)
+    response = genai.generate_text(prompt=full_prompt)
+    ans = response.result.strip().lower().replace('*','')
+    intents[ans]
--- website/hospital/chatbot/symptom_doc_matcher.py
+++ website/hospital/chatbot/symptom_doc_matcher.py
@@ -1,20 +1,21 @@
-from langchain_google_genai import GoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+import google.generativeai as genai
 import os
 
-llm = GoogleGenerativeAI(google_api_key = os.environ["best_offer"], model=os.environ["model"])
+genai.configure(api_key=os.environ["best_offer"])
+model = genai.GenerativeModel('gemini-1.5-flash')
 
-prompt = PromptTemplate.from_template('''check the following sentence and if the sentence is about medical symptoms then tell me what type of doctor
+common_prompt = '''check the following sentence and if the sentence is about medical symptoms then tell me what type of doctor
  should i suggest and the doctor type should be among these("General practitioner", "physician", "cardiologist", "gastroenterologist", "dermatologist",
  "neurologist", "orthopedic", "pediatrician", "gynecologist", "obstetrician", "urologist", "nephrologist", "psychiatrist", "dentist", "maxillofacial",
  "physiotherapist", "ophthalmologist", "allergist", "immunologist", "pulmonologist", "endocrinologist", "ENT Specialist")based on the above info only respond in the intents as I mentioned above,
-  or if the sentence is not about symptoms say 'nope'. The sentence is '{symptoms}' ''')
+  or if the sentence is not about symptoms say 'nope'. The sentence is '{symptoms}' '''
 
 doctors = ["general practitioner", "physician", "cardiologist", "gastroenterologist", "dermatologist", "neurologist", "orthopedic", "pediatrician", "gynecologist", "obstetrician", "urologist", "nephrologist", "psychiatrist", "dentist", "maxillofacial", "physiotherapist", "ophthalmologist", "allergist", "immunologist", "pulmonologist", "endocrinologist", "ent specialist"]
 
 def predict_doc(symptoms):
-    chain = prompt | llm
-    ans = chain.invoke(symptoms).strip().lower().replace('*','')
+    full_prompt = common_prompt.format(symptoms=symptoms)
+    response = model.generate_content(full_prompt)
+    ans = response.candidates[0].content.parts[0].text.strip().lower().replace('*', '')
     
     if ans == 'nope':
         return 0
@@ -23,5 +24,6 @@ def predict_doc(symptoms):
     if ans == "immunologist" or ans == "obstetrician" or ans == "maxillofacial":
         ind-=1
     return ind+1
-    
+
+
 
